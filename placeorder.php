b482a17b4e46a4f341a4e19d6d14f819d9ad7f49
Sultan1992 <sultanfeto1>
2020-05-21 03:23:05 -0700

    comment removed

--- placeorder.php
+++ placeorder.php
@@ -1,5 +1,4 @@
 <?=template_header('Place Order')?>
-//wrap the order
 <div class="placeorder content-wrapper">
     <h1>Your Order Has Been Placed</h1>
     <p>Thank you for ordering with us, we'll contact you by email with your order details.</p>
